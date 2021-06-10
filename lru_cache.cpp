46c4f50ede592315b6b892310dd75aa9e39dcef7
arv-pyrna <arv.pyrna@gmail.com>
2021-06-11 06:06:41 +0700

    Changes made on: Пт 11 июн 2021 06:06:41 +07

--- lru_cache.cpp
+++ lru_cache.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <unordered_map>
 
+#include "containers/linked_list.h"
+
 /*
 ToDo:
 - Iterators
@@ -13,16 +15,42 @@ ToDo:
 template <typename Key, typename Value>
 class LRUCache {
 public:
-    Value &operator[](Key key) {
-        return 0;
+    explicit LRUCache(size_t n = 10) {
+        threshold_ = n;
     }
 
-private:
+    ~LRUCache() {
+        // Free linked list
+    }
 
+    Value &operator[](Key key) {
+        return table_[key].value;
+    }
+
+    void Clear() {
+        size_ = 0;
+        table_.clear();
+    }
+
+    // return element on the top
+    // Value peek(){
+    //     return table_[].value;
+    // }
+
+private:
+    struct Node {};
+    struct Data {
+        Value value;
+        Node *node;
+    };
+
+    std::unordered_map<Key, Data> table_;
+    size_t size_;
+    size_t threshold_;
 };
 
 int main() {
-    LRUCache<int, int> cache;
+    LRUCache<int, int> cache(10);
     cache[10] = 100;
     //     cache[10] = 10;
     return 0;
