9c07bd09a37245b25621de5896c82513655bbdd5
arv-pyrna <arv.pyrna@gmail.com>
2021-06-12 18:28:08 +0700

    bloom filter changes

--- bloom_filter.cpp
+++ bloom_filter.cpp
@@ -1,4 +1,20 @@
+#include "bloom_filter.h"
+
 #include <iostream>
+
+/*
+SPEC
+****
+
+use uniformly distributed hash function for BloomFilter
+
+*/
+
+template <typename Key, typename Value>
+BloomFilter<Key, Value>::BloomFilter(const size_t n) {
+}
+
 int main() {
+    // BloomFilter b_filter(10);
     return 0;
 }
--- bloom_filter.h
+++ bloom_filter.h
@@ -1,20 +1,23 @@
-
 #ifndef bloom_filter_h
 #define bloom_filter_h
+
+#include <bitset>
 
 template <typename Key, typename Value>
 class BloomFilter {
 public:
-    BloomFilter(size_t n);
-	Value &operator[](Key key);
-	bool Delete(Key key);
-	bool Empty();
+    BloomFilter(const size_t n);
+    Value &operator[](Key key);
+    bool Delete(Key key);
+    bool Empty();
     void Clear();
-	Value Peek(); // Returns the head of the cache
+    Value Peek();  // Returns the head of the cache
     size_t Size();
+    void Print();
 
 private:
     size_t size_;
+	std::bitset<0> store;
 };
 
 #endif /* bloom_filter_h */
