37d97fc33463c24bb3262c276cfc8d7105b76bdc
arvyshka <arv.pyrna@gmail.com>
2021-09-13 10:37:26 +0300

    Changes made on: Пн 13 сен 2021 10:37:26 MSK

--- main.cpp
+++ main.cpp
@@ -1,30 +1,19 @@
-// https://codeforces.com/contest/1549/problem/C
-
+#include <algorithm>
+#include <vector>
 #include <iostream>
 
-struct Node{
-	int id;
-	Node* frnd;
-};
-
-void removeLink(int n1, int n2){
-
-}
-
-void addLink(int n1, int n2){
-
+bool containsDuplicate(std::vector<int>& nums) {
+  std::sort(nums.begin(), nums.end());
+  for(size_t i=0; i<nums.size(); i++){
+    if(i != nums.size()-1 && nums[i] == nums[i+1]){
+      return true;
+    }
+  }
+  return false;
 }
 
 int main() {
-    int n = 0, f = 0;
-    int n1 = 0, n2 = 0;
-    std::cin >> n;
-    std::cin >> f;
-    for (int i = 0; i < f; i++) {
-		std::cin >> n1;
-		std::cin >> n2;
-
-
-    }
+    std::vector<int> vect{50, 30, 11, 20, 51};
+    std::cout << containsDuplicate(vect);
     return 0;
 }
